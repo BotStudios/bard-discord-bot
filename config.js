7ac30fc5806d2f2be904e9b048f62a9ab0e77418
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2023-04-29 00:45:38 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+    cookie: "COOKIE", // "__Secure-1PSID" cookie value form bard.google.com
+    token: "TOKEN", // discord.com/developer (Discord Bot Token)
+    messageCommand: false // "true" to enable "!ask" command (requires MessageContent intent)
+}
